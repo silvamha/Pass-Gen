009a44386b7d2f2e2785f0e8a93f85fb7ffa67d0
Ed <silva.email@yahoo.com>
2024-09-20 20:37:05 -0500

    Uldated random pass code

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-let app = document.querySelector("#app");
-let password = document.querySelector("#password");
-let passwordLength = 12;
-let passwordLowercase = true;
-let passwordUppercase = true;
-let passwordNumbers = true;
-let passwordSymbols = true;
+const appEl = document.querySelector("#app-el");
+const passEl = document.querySelector("#pass-el");
 
-let body = document.querySelector("body");
+const genPass = (length) => {
 
-function generatePassword(length) {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
-  const symbols = "!@#$%^&*()_-+={}[]|;:,.<>?";
+  const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
   let password = "";
+  
+  console.log(upperCase.charAt(Math.floor(Math.random() * upperCase.length)));
+};
 
-  // Ensure we have at least one of each type
-  password += letters.charAt(Math.floor(Math.random() * letters.length));
-  password += numbers.charAt(Math.floor(Math.random() * numbers.length));
-  password += symbols.charAt(Math.floor(Math.random() * symbols.length));
-
-  // Fill the rest of the password with random characters
-  for (let i = 3; i < length; i++) {
-    const allChars = letters + numbers + symbols;
-    const randomIndex = Math.floor(Math.random() * allChars.length);
-    password += allChars.charAt(randomIndex);
-    console.log(allChars)
-  }
-
-  // Shuffle the password for extra security
-  password = password
-    .split("")
-    .sort(() => 0.5 - Math.random())
-    .join("");
-  return password;
-}
-
-// Example usage
-let newPassword = generatePassword(12);
-console.log(newPassword);
+// for (let i = 0; i < upperCase.length; i++) {}
